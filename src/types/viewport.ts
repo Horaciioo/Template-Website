681fc1f50113529c932a7463eee224aca5906948
Jérémy <aime.jeremys@gmail.com>
2026-08-12 20:51:09 +0200

    🔥 Removed the unused ViewportService

--- src/types/viewport.ts
+++
@@ -1,5 +0,0 @@
-import type viewport from '@/configurations/system/viewport.json'
-import type { RegistryKey } from '@/types/common'
-
-export type BreakpointName = RegistryKey<typeof viewport.breakpoints>
-export type MediaQueryName = RegistryKey<typeof viewport.mediaQueries>
